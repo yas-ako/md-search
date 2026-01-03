274eba535b853a52f7ab19d9d83cf8bcb7eb3eae
yas-ako <105139975+yas-ako@users.noreply.github.com>
2026-01-03 19:19:40 +0900

    - CodiMDで数式をレンダリング - Markdownのスタイルをgithub-light に

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: 'github-light',
+    },
+    // 数式レンダリングを有効化
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+});
